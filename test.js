69ce9bb26e9ae68711bf7b09eeabd5724749c2e7
hf-mybook <308171220@qq.com>
2023-02-17 00:01:26 +0800

    修改bug
    Signed-off-by: hf-mybook <308171220@qq.com>

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
-let parStr = 'Java javaScript Ja JAHAHjaeerewf'
-let keyWord = 'Jav'
+// let parStr = 'Java javaScript Ja JAHAHjaeerewf'
+let parStr = '  wf'
+let keyWord = ' WF'
 function fn(parStr, keyWord, spanStart, spanEnd) {
   let arr = []
   let arr2 = []
@@ -11,8 +12,8 @@ function fn(parStr, keyWord, spanStart, spanEnd) {
   console.log('arr:', arr)
 
   arr.forEach((item, index) => {
-    let endIndex = item + keyWord.length
-    let result = parStr.slice(item, endIndex)
+    let endIndex = item + keyWord.length - 1
+    let result = parStr.slice(item, endIndex + 1)
     console.log('result:', result)
     if (index == 0) {
       let aaa = parStr.slice(0, item) + spanStart + result + spanEnd
@@ -23,11 +24,12 @@ function fn(parStr, keyWord, spanStart, spanEnd) {
       arr2.push(aaa)
       console.log('beforeItem:', beforeItem)
     }
-    if (index == arr.length - 1) {
-      arr2.push(parStr.substring(endIndex))
+    if (index == arr.length - 1 && endIndex != parStr.length - 1) {
+      arr2.push(parStr.substring(endIndex + 1))
     }
   })
   console.log('arr2:', arr2)
 
   return arr2.join('')
 }
+fn(parStr, keyWord,'<div>','</div>')
